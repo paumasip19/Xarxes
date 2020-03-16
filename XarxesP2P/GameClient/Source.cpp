69658f6a06099f2f76a2ff887659b02cc0e34894
Pau <pau.masip@enti.cat>
2020-03-16 01:52:12 +0100

    Update
    - Se pueden hacer turnos de tiradas normales
    - Se respetan los turnos
    - Se actualizan los personajes en pantalla de todos

--- XarxesP2P/GameClient/Source.cpp
+++ XarxesP2P/GameClient/Source.cpp
@@ -105,8 +105,10 @@ int main()
 	srand(time(NULL));
 
 	bool running = true;
-
 	bool amIFirst = false;
+	int myPosition = 0;
+	int nextPlayer = 0;
+	int prevPlayer = 0;
 
 	std::string mens = "_";
 
@@ -147,6 +149,8 @@ int main()
 				{
 					amIFirst = true;
 				}
+
+				myPosition = size;
 
 				for (int i = 0; i < size; i++)
 				{
@@ -200,6 +204,27 @@ int main()
 		}
 	}
 
+	//Calcular jugador siguiente y anterior
+	if (myPosition == peers.size()) //Revisar
+	{
+		nextPlayer = 0;
+	}
+	else
+	{
+		nextPlayer = myPosition;
+	}
+
+	if (myPosition == 0)
+	{
+		prevPlayer = peers.size();
+	}
+	else
+	{
+		prevPlayer = myPosition - 1;
+	}
+
+
+
 	//Nos hemos conectado todos bien
 	//INICIO DE PARTIDA
 	std::cout << "Comienza la partida!!!" << std::endl;
@@ -231,6 +256,7 @@ int main()
 		std::string delimiter2;
 		std::string temp;
 		std::string temp2;
+		std::string tempC;
 
 		size_t pos = 0;
 
@@ -255,7 +281,7 @@ int main()
 		}
 
 		if (amIFirst)
-		{		
+		{
 			//Envia info inicial
 			std::vector<Carta> solucion;
 
@@ -271,9 +297,8 @@ int main()
 			peers.push_back(Client());
 			inicializarJuego(barajaCompleta, solucion, peers, avatares);
 
-			for (int i = 0; i < peers.size() - 1; i++)
+			for (int i = 0; i < peers.size(); i++)
 			{
-				std::string temp;
 
 				for (int j = 0; j < peers[i].player.mano.size(); j++)
 				{
@@ -302,7 +327,48 @@ int main()
 				temp += "/";
 
 				c = Cabezera::INITIALIZEPLAYER;
-				Send(peers[i], c, temp);
+				if (i != peers.size() - 1)
+				{
+					Send(peers[i], c, temp);
+				}				
+			}
+
+			m = temp;
+
+			delimiter1 = "-";
+			delimiter2 = "/";
+			temp = "";
+
+			while ((pos = m.find(delimiter1)) != std::string::npos) {
+				std::string temp = m.substr(0, pos);
+				m.erase(0, pos + delimiter1.length());
+			}
+
+			m.erase(m.begin());
+
+			while ((pos = m.find(delimiter2)) != std::string::npos) {
+				std::string temp = m.substr(0, pos);
+				m.erase(0, pos + delimiter2.length());
+
+				if (temp == "000")
+				{
+					p.push_back(GraphicPlayer(sf::Color::Black, sf::Vector2f(28 * SIZE, 29 * SIZE)));
+				}
+				else if (temp == "02550") {
+					p.push_back(GraphicPlayer(sf::Color::Green, sf::Vector2f(11 * SIZE, 29 * SIZE)));
+				}
+				else if (temp == "2550255") {
+					p.push_back(GraphicPlayer(sf::Color::Magenta, sf::Vector2f(0 * SIZE, 10 * SIZE)));
+				}
+				else if (temp == "00255") {
+					p.push_back(GraphicPlayer(sf::Color::Blue, sf::Vector2f(19 * SIZE, 0 * SIZE)));
+				}
+				else if (temp == "25500") {
+					p.push_back(GraphicPlayer(sf::Color::Red, sf::Vector2f(30 * SIZE, 0 * SIZE)));
+				}
+				else if (temp == "2552550") {
+					p.push_back(GraphicPlayer(sf::Color::Yellow, sf::Vector2f(39 * SIZE, 10 * SIZE)));
+				}
 			}
 
 			player = peers.back().player;
@@ -314,61 +380,236 @@ int main()
 			g = Graphics(p);
 
 			amIFirst = false;
+
+			g.DrawDungeon(_window, shape);
+
+			c = Cabezera::YOURTURNDICE;
 		}
-		
+
 		switch (c)
 		{
-			case Cabezera::INITIALIZEPLAYER:
-				delimiter1 = "-";
-				delimiter2 = "/";
-				temp = "";
+		case Cabezera::INITIALIZEPLAYER:
+			delimiter1 = "-";
+			delimiter2 = "/";
+			temp = "";
 
-				while ((pos = m.find(delimiter1)) != std::string::npos) {
-					std::string temp = m.substr(0, pos);
-					m.erase(0, pos + delimiter1.length());
+			while ((pos = m.find(delimiter1)) != std::string::npos) {
+				std::string temp = m.substr(0, pos);
+				m.erase(0, pos + delimiter1.length());
 
-					player.mano.push_back(Carta(TipoCarta((int)(temp[0] - 48)), temp.substr(1, temp.length() - 1)));
+				player.mano.push_back(Carta(TipoCarta((int)(temp[0] - 48)), temp.substr(1, temp.length() - 1)));
+			}
+
+			m.erase(m.begin());
+
+			while ((pos = m.find(delimiter2)) != std::string::npos) {
+				std::string temp = m.substr(0, pos);
+				m.erase(0, pos + delimiter2.length());
+
+				if (temp == "000")
+				{
+					p.push_back(GraphicPlayer(sf::Color::Black, sf::Vector2f(28 * SIZE, 29 * SIZE)));
 				}
+				else if (temp == "02550") {
+					p.push_back(GraphicPlayer(sf::Color::Green, sf::Vector2f(11 * SIZE, 29 * SIZE)));
+				}
+				else if (temp == "2550255") {
+					p.push_back(GraphicPlayer(sf::Color::Magenta, sf::Vector2f(0 * SIZE, 10 * SIZE)));
+				}
+				else if (temp == "00255") {
+					p.push_back(GraphicPlayer(sf::Color::Blue, sf::Vector2f(19 * SIZE, 0 * SIZE)));
+				}
+				else if (temp == "25500") {
+					p.push_back(GraphicPlayer(sf::Color::Red, sf::Vector2f(30 * SIZE, 0 * SIZE)));
+				}
+				else if (temp == "2552550") {
+					p.push_back(GraphicPlayer(sf::Color::Yellow, sf::Vector2f(39 * SIZE, 10 * SIZE)));
+				}
+			}
 
-				m.erase(m.begin());
+			player.avatar = p[0].shape.getFillColor();
+			printearPlayer(player);
+			printearCartas(player);
 
-				while ((pos = m.find(delimiter2)) != std::string::npos) {
-					std::string temp = m.substr(0, pos);
-					m.erase(0, pos + delimiter2.length());
+			g = Graphics(p);
 
-					if (temp == "000")
+			g.DrawDungeon(_window, shape);
+
+			c = Cabezera::COUNTT;
+
+			break;
+
+		case Cabezera::YOURTURNDICE:
+			system("cls");
+			printearPlayer(player);
+			printearCartas(player);
+
+			std::cout << "Es tu turno. Escribe 'R' para tirar" << std::endl;
+
+			while (t != "R")
+			{
+				std::cin >> t;
+				if (t == "R")
+				{
+					dice[0] = rand() % 6 + 1;
+					dice[1] = rand() % 6 + 1;
+					std::cout << "Has sacado un " + std::to_string(dice[0]) + " y un " + std::to_string(dice[1]) + " !!!";
+					dice[0] += dice[1];
+					std::cout << "Mueve tu personaje " + std::to_string(dice[0]) + " veces" << std::endl;
+					int movimientos = dice[0];
+					sf::Vector2f lastPos = g.gPlayers[0].shape.getPosition();
+					g.canMove = true;
+					while (movimientos != 0)
 					{
-						p.push_back(GraphicPlayer(sf::Color::Black, sf::Vector2f(28 * SIZE, 29 * SIZE)));
+						if (lastPos != g.gPlayers[0].shape.getPosition())
+						{
+							movimientos--;
+							lastPos = g.gPlayers[0].shape.getPosition();
+						}
+
+						g.DrawDungeon(_window, shape);
 					}
-					else if (temp == "02550") {
-						p.push_back(GraphicPlayer(sf::Color::Green, sf::Vector2f(11 * SIZE, 29 * SIZE)));
+					g.canMove = false;
+
+					m = std::to_string(g.gPlayers[0].shape.getPosition().x) + "-" + std::to_string(g.gPlayers[0].shape.getPosition().y);
+
+					m += "-/";
+
+					m += std::to_string(player.avatar.r);
+					m += std::to_string(player.avatar.g);
+					m += std::to_string(player.avatar.b);
+
+					c = Cabezera::GLOBALTURNDICE;
+					Send(peers, c, m);
+
+					if (g.checkearSalas(salaActual))
+					{
+						while (eleccion != "1" && eleccion != "2")
+						{
+							std::cout << "Ahora, te encuentras en la " << salaActual << std::endl;
+							std::cout << "Que quieres hacer?" << std::endl;
+							std::cout << "1. Hacer sugestion" << std::endl;
+							std::cout << "2. Hacer resolucion" << std::endl;
+
+							std::cin >> eleccion;
+							if (eleccion == "1")
+							{
+								/*m = hacerAcusacion(barajaCompleta, salaActual);
+								c = Cabezera::YOURTURNCARDS;
+								Send(servidor, c, m, running);*/
+							}
+							else if (eleccion == "2")
+							{
+								/*m = hacerAcusacionFinal(barajaCompleta);
+								c = Cabezera::MAKERESOLUTION;
+								Send(servidor, c, m, running);*/
+							}
+							else
+							{
+								system("cls");
+								std::cout << "Vuelve a intentarlo." << std::endl;
+							}
+						}
 					}
-					else if (temp == "2550255") {
-						p.push_back(GraphicPlayer(sf::Color::Magenta, sf::Vector2f(0 * SIZE, 10 * SIZE)));
+					else
+					{
+						while (eleccion != "1" && eleccion != "2")
+						{
+							std::cout << "Que quieres hacer?" << std::endl;
+							std::cout << "1. Hacer resolucion" << std::endl;
+							std::cout << "2. Pasar turno" << std::endl;
+
+							std::cin >> eleccion;
+							if (eleccion == "1")
+							{
+								/*m = hacerAcusacionFinal(barajaCompleta);
+								c = Cabezera::MAKERESOLUTION;
+								Send(servidor, c, m, running);*/
+							}
+							else if (eleccion == "2")
+							{
+								c = Cabezera::YOURTURNDICE;
+								m = "";
+
+								Send(peers[nextPlayer], c, m);
+								
+							}
+							else
+							{
+								system("cls");
+								std::cout << "Vuelve a intentarlo." << std::endl;
+							}
+						}
 					}
-					else if (temp == "00255") {
-						p.push_back(GraphicPlayer(sf::Color::Blue, sf::Vector2f(19 * SIZE, 0 * SIZE)));
-					}
-					else if (temp == "25500") {
-						p.push_back(GraphicPlayer(sf::Color::Red, sf::Vector2f(30 * SIZE, 0 * SIZE)));
-					}
-					else if (temp == "2552550") {
-						p.push_back(GraphicPlayer(sf::Color::Yellow, sf::Vector2f(39 * SIZE, 10 * SIZE)));
+				}
+				else
+				{
+					std::cout << "Nos has escrito R. Vuleve a intenatarlo:" << std::endl;
+				}
+
+				c = Cabezera::COUNTT;
+			}
+			break;
+		case Cabezera::GLOBALTURNDICE:
+			delimiter1 = "-";
+			delimiter2 = "/";
+
+			while ((pos = m.find(delimiter1)) != std::string::npos) {
+				std::string temp = m.substr(0, pos);
+				m.erase(0, (pos + 1) + (delimiter1.length() - 1));
+
+				if (z == 0)
+				{
+					position = { std::stof(temp), std::stof(m) };
+					z++;
+				}
+
+			}
+
+			while ((pos = m.find(delimiter2)) != std::string::npos) {
+				tempC = m.substr(0, pos);
+				m.erase(0, pos + delimiter2.length());
+
+				if (m == "000") {
+					colorTemp = sf::Color::Black;
+				}
+				else if (m == "02550") {
+					colorTemp = sf::Color::Green;
+				}
+				else if (m == "2550255") {
+					colorTemp = sf::Color::Magenta;
+				}
+				else if (m == "00255") {
+					colorTemp = sf::Color::Blue;
+				}
+				else if (m == "25500") {
+					colorTemp = sf::Color::Red;
+				}
+				else if (m == "2552550") {
+					colorTemp = sf::Color::Yellow;
+				}
+
+				for (int i = 0; i < g.gPlayers.size(); i++)
+				{
+					if (g.gPlayers[i].shape.getFillColor() == colorTemp)
+					{
+						g.gPlayers[i].shape.setPosition(position);
 					}
 				}
 
-				player.avatar = p[0].shape.getFillColor();
-				printearPlayer(player);
-				printearCartas(player);
+				isInSala = g.checkearSalasOther(colorTemp, salaActual, p);
 
-				g = Graphics(p);
+				printearMovimientoExterno(colorTemp, isInSala, salaActual);
+
+				c = Cabezera::COUNTT;
 				break;
 			default:
 				break;
-		}
+			}
 
-		c = Cabezera::COUNTT;
-		g.DrawDungeon(_window, shape);
+			c = Cabezera::COUNTT;
+			g.DrawDungeon(_window, shape);
+		}
 	}
 }
 
